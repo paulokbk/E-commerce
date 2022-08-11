5fbf0aa70cc4d4bef74b343c408cf58af35b0537
Paulo Filho <paulokbkfilho@hotmail.com>
2022-08-11 11:47:01 -0300

    Atualizações visual no header

--- models/vivara.js
+++ models/vivara.js
@@ -1,9 +1,9 @@
 const data =
 [
     {
-    codigo: 1,
-    imagem1: 'produto001.webp',
-    imagem2:  'produto001-b.webp' ,
+    id: 1,
+    image1: 'produto001.webp',
+    image2:  'produto001-b.webp' ,
     nome: 'Perfume Masculino V - Eau de Parfum 100ml',
     preco: '310',
     volume: '100ml',
@@ -16,9 +16,9 @@ const data =
     garantia: '3 meses'
 },
 {
-    codigo: 2,
-    imagem1: 'produto001.webp',
-    imagem2:  'produto001-b.webp' ,
+    id: 2,
+    image1: 'produto001.webp',
+    image2:  'produto001-b.webp' ,
     nome: 'Perfume Masculino V - Eau de Parfum 100ml',
     preco: '310',
     volume: '100ml',
@@ -31,9 +31,9 @@ const data =
     garantia: '3 meses'
 },
 {
-    codigo: 3,
-    imagem1: 'produto001.webp',
-    imagem2:  'produto001-b.webp' ,
+    id: 3,
+    image1: 'produto001.webp',
+    image2:  'produto001-b.webp' ,
     nome: 'Perfume Masculino V - Eau de Parfum 100ml',
     preco: '310',
     volume: '100ml',
@@ -46,9 +46,9 @@ const data =
     garantia: '3 meses'
 },
 {
-    codigo: 4,
-    imagem1: 'produto001.webp',
-    imagem2:  'produto001-b.webp' ,
+    id: 4,
+    image1: 'produto001.webp',
+    image2:  'produto001-b.webp' ,
     nome: 'Perfume Masculino V - Eau de Parfum 100ml',
     preco: '310',
     volume: '100ml',
@@ -61,9 +61,9 @@ const data =
     garantia: '3 meses'
 },
 {
-    codigo: 5,
-    imagem1: 'produto001.webp',
-    imagem2:  'produto001-b.webp' ,
+    id: 5,
+    image1: 'produto001.webp',
+    image2:  'produto001-b.webp' ,
     nome: 'Perfume Masculino V - Eau de Parfum 100ml',
     preco: '310',
     volume: '100ml',
@@ -76,9 +76,9 @@ const data =
     garantia: '3 meses'
 },
 {
-    codigo: 6,
-    imagem1: 'produto001.webp',
-    imagem2:  'produto001-b.webp' ,
+    id: 6,
+    image1: 'produto001.webp',
+    image2:  'produto001-b.webp' ,
     nome: 'Perfume Masculino V - Eau de Parfum 100ml',
     preco: '310',
     volume: '100ml',
@@ -90,5 +90,33 @@ const data =
     tipo: ['acessorios', 'masculino'],
     garantia: '3 meses'
 },
-
 ]
+
+const vivara = {
+    //1 - pegar todos os pets
+    getAll: () =>{
+        return data
+    },
+    //2 - pegar pelo tipo do produto
+    getFromType:(tipo) =>{
+        return data.filter(item =>{
+            for(let i = 0; i < item.tipo.length; i++){
+                if(item.tipo[i] === tipo){
+                    return true
+                 } else{
+                     false
+            }
+        }})
+    },
+    // // 3 - pegar pelo nome do pet
+    // getFromName: (name) =>{
+    //     return data.filter(item => {
+    //         if(item.name.indexOf(name) > -1){
+    //             return true
+    //         } else{
+    //             return false
+    //         }
+    //     })
+    // }
+}
+module.exports.vivara = vivara
