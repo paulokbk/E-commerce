d3382b44ed009286da0a25a2a34376d913504087
Paulo Filho <paulokbkfilho@hotmail.com>
2022-08-15 11:36:40 -0300

    colocado filtro provisorio nas paginas do nav

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,48 +1,60 @@
 const {createMenuObject} = require('../helpers/createMenuObject')
+const {vivara} = require('../models/vivara')
 
 const joias = (req, res) => {
+    let lista = vivara.getAll()
+    // console.log("lista", lista)
     res.render('pages/index',{
         menu: createMenuObject('joias'),
         banner: {
             background: 'fulbanner_home.webp',
             typePage: 'home'
-        }
+        },
+        lista
     })
 }
 
 const casamento = (req, res) => {
+    let lista = vivara.getAll()
     res.render('pages/index',{
         menu: createMenuObject('casamento'),
         banner: {
             background: 'fullbanner-casamento.webp',
-        }
+        },
+        lista
     })
 }
 
 const relogios = (req, res) => {
+    let lista = vivara.getAll()
     res.render('pages/index',{
         menu: createMenuObject('relogios'),
         banner: {
             background: 'fullbanner_relogios_vivara_2560_1024.webp',
-        }
+        },
+        lista
     })
 }
 
 const acessorios = (req, res) => {
+    let lista = vivara.getAll()
     res.render('pages/index',{
         menu: createMenuObject('acessorios'),
         banner: {
             background: 'fullbanner_acessorios_oculos_2560_1024.webp',
-        }
+        },
+        lista
     })
 }
 
 const masculino = (req, res) => {
+    let lista = vivara.getAll()
     res.render('pages/index',{
         menu: createMenuObject('masculino'),
         banner: {
             background: 'fullbanner-masculino-acessorios.webp',
-        }
+        },
+        lista
     })
 }
 
