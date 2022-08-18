db314dd7ab8c4568d0d86725f36aee30c1e2fbb8
Alysson Cruz <alysson.cruz1@outlook.com>
2022-08-18 15:30:52 -0300

    Criação do controller para a rota checkout

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,14 @@
+const {createMenuObject} = require('../helpers/createMenuObject')
+
 const login = (req, res) => {
     res.render ('pages/login')
 }
 
+const checkout = (req, res) => {
+
+    res.render ('pages/checkout', {menu: createMenuObject('false')})
+
+}
+
 module.exports.login = login
+module.exports.checkout = checkout
