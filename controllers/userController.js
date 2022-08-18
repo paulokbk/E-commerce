53abff5bb3dc9075b5fc3c4f8f8ffb886866b813
maluquerino <marialsq25@hotmail.com>
2022-08-17 21:56:46 -0300

    Cadastro no ejs

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,17 @@
+const {createMenuObject} = require('../helpers/createMenuObject')
+
 const login = (req, res) => {
     res.render ('pages/login')
 }
 
+
+
+const cadastro = (req, res) => {
+    res.render('pages/cadastro',{
+       menu: createMenuObject('false'),
+    })
+}
+
+
 module.exports.login = login
+module.exports.cadastro = cadastro
