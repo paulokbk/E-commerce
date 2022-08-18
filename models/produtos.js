093091d4c0d6b8bdcc70e48430cc446c7da67d13
Paulo Filho <paulokbkfilho@hotmail.com>
2022-08-18 16:21:37 -0300

    criação de função para escrever no banco em json

---
+++ models/produtos.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const {v4} = require('uuid');
+
+
+let db_produtos = require('../database/db_produtos.json');
+
+
+const writeToDB = () =>{
+    const json = JSON.stringify(db_produtos);
+    fs.writeFileSync('database/db_produtos.json' , json);
+}
+
+const Produto = {
+    criar: (produto, imagem) =>{
+        db_produtos.produtos.push({ id: v4(), ...produto, imagem});
+        writeToDB();
+    }
+}
+
+module.exports = Produto;
