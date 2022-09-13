4b1f859128fc05f4dc7e458aa43822c1886c6454
Paulo Filho <paulokbkfilho@hotmail.com>
2022-09-13 16:45:26 -0300

    correção erro de exportação

---
+++ models/Filtros.js
@@ -0,0 +1,44 @@
+let db = require('../database/db_produtos.json');
+
+const Filtros = {
+    findAll: () => db.produtos,
+
+    findByCategoria: (tipo) => {
+        return db.produtos.filter( produto =>{
+            for(let i = 0; i < produto.tipos.length; i++){
+                if(produto.tipos[i] == tipo){
+                        return produto
+                }
+            }
+        })  
+    },
+
+    findByNome: (nome) => {
+        return db.produtos.filter(produto => {
+            if (produto.nome.toLowerCase().indexOf(nome.toLowerCase()) > -1) {
+                return true
+            } else {
+                return false
+            }
+        })
+    },
+
+    findByid: (id) =>{
+        return db.produtos.filter(produto =>{
+            if(produto.id == id){
+                return true
+            }else{
+                return false
+            }
+        })
+    },
+
+    FindBestSeller: () => {
+        return db.produtos.filter(produto =>{
+            if(produto.bestseller){
+                return produto
+            }
+        })
+    },
+}
+module.exports = Filtros;
