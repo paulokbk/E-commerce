88f956f5b6279f4f8f46e590ab3715a1c4620dfe
Paulo Filho <paulokbkfilho@hotmail.com>
2022-09-13 11:40:39 -0300

    criação do controller da tela de busca

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,5 +1,17 @@
-const search = (req, res) => {
+const {createMenuObject} = require('../helpers/createMenuObject')
+const { findByNome } = require("../models/Usuario");
 
+const searchController = {
+    buscar: (req, res) => {
+        let busca =  req.query.busca;
+        let resultado_busca = findByNome(busca);
+        res.render('pages/busca', {
+            menu: createMenuObject('false'),
+            resultado_busca,
+            busca
+        })
+    }
 }
 
-module.exports.search = search;
+
+module.exports = searchController;
