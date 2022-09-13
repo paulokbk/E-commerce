2ba22cbfd4108afb968610a971537a2802cd4548
Paulo Filho <paulokbkfilho@hotmail.com>
2022-09-13 16:46:25 -0300

    criando novo metodo para mostrar item

--- models/produtos.js
+++ models/produtos.js
@@ -12,26 +12,6 @@ const writeToDB = () =>{
 
 const Produto = {
 
-    findAll: () => db_produtos.produtos,
-
-    bestSeller: () => {
-        return db_produtos.produtos.filter(produto =>{
-            if(produto.bestseller){
-                return produto
-            }
-        })
-    },
-
-    findByCategoria: (tipo) => {
-        return db_produtos.produtos.filter( produto =>{
-            for(let i = 0; i < produto.tipos.length; i++){
-                if(produto.tipos[i] == tipo){
-                        return produto
-                }
-            }
-        })  
-    },
-
     criar: (produto, imagem) =>{
         db_produtos.produtos.push({ id: v4(), ...produto, imagem});
         writeToDB();
