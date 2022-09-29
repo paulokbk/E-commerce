74d2b54ac1e10ba1ab4895133ea70b75b39956ad
Paulo Filho <paulokbkfilho@hotmail.com>
2022-09-28 21:58:09 -0300

    altereção de rotas de produtos para admins

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const adminController = require('../controllers/adminController');
 const produtoController = require('../controllers/produtoController');
 const multer = require('multer');
-const { storage } =  require('../config/upload')
+const { storage } =  require('../config/upload');
+const authentification = require('../middlewares/authentication');
+const authentication = require('../middlewares/authentication');
+
+
+//Rotas para login adm (CRUD produtos)
+
+router.get('/login', adminController.formLogin);
+router.post('/login', adminController.login);
+router.post('/logout', adminController.logout);
 
 //inicialização do multer com as configurações de storagee
 const upload = multer({ storage });
 
 
 //rota para listar todos produtos
-router.get('/', produtoController.index);
+router.get('/', authentication , produtoController.index);
 
 //rota para mostrar o cadastro de produtos
 router.get('/adicionar', produtoController.criarFormulario);
@@ -23,5 +33,8 @@ router.put('/:id', upload.single('imagem') , produtoController.editarProduto);
 
 // rota para deletar produto
 router.delete('/:id', produtoController.deletarProduto)
+
+
+
 
 module.exports = router;
